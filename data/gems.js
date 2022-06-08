b4d8de56f2b77ab4bed038ef91cf494c30b6a9cd
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-08 16:37:29 -0700

    forgot my initial commit... filled out controllers folder, setup.sql, models folder

---
+++ data/gems.js
@@ -0,0 +1,82 @@
+const gems = [
+    {
+        id: '1',
+        name: 'Steven',
+        nickname: 'Schtu-ball',
+        species: 'Part Human, Part Gem',
+        weapon: 'Shield',
+        components: 'Not a fusion',
+        url: 
+
+    },
+    {
+        id: '2',
+        name: 'Connie',
+        nickname: 'Miss Knight',
+        species: 'Human',
+        weapon: 'Sword',
+        components: 'Not a fusion',
+        url: 
+
+    },
+    {
+        id: '3',
+        name: 'Stevonnie',
+        nickname: 'Dondai',
+        species: 'Part Human, Part Gem',
+        weapon: 'Shield & Sword',
+        components: 'Steven, Connie',
+        url: 
+
+    },
+    {
+        id: '4',
+        name: 'Garnet',
+        nickname: 'The boss',
+        species: 'Gem',
+        weapon: 'Gauntlets',
+        components: 'Ruby, Sapphire',
+        url:  
+
+    },
+    {
+        id: '5',
+        name: 'Amethyst',
+        nickname: 'Ams',
+        species: 'Gem',
+        weapon: 'Whip',
+        components: 'Not a fusion',
+        url:  
+
+    },
+    {
+        id: '6',
+        name: 'Pearl',
+        nickname: 'P',
+        species: 'Gem',
+        weapon: 'Spear, Swords',
+        components: 'Not a fusion',
+        url: 
+
+    },
+    {
+        id: '7',
+        name: 'Peridot',
+        nickname: 'Peri',
+        species: 'Gem',
+        weapon: 'Limb-enhancers',
+        components: 'Not a fusion',
+        url:
+    },
+    {
+        id: '8',
+        name: 'Lapis Lazuli',
+        nickname: 'Lapis',
+        species: 'Gem',
+        weapon: 'Hydrokinesis',
+        components: 'Not a fusion',
+        url: 
+
+    },
+   
+]
